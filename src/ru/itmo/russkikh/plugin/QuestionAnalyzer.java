06b4d7c92adf37265132484dafcf0dadb04b2672
polina <greenowl403@gmail.com>
2020-07-02 19:41:05 +0300

    Upgrade

--- src/ru/itmo/russkikh/plugin/QuestionAnalyzer.java
+++ src/ru/itmo/russkikh/plugin/QuestionAnalyzer.java
@@ -1,33 +1,23 @@
 package ru.itmo.russkikh.plugin;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ContentIterator;
 import com.intellij.openapi.roots.ProjectFileIndex;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.PsiManager;
-import com.intellij.psi.impl.source.JavaFileElementType;
 import com.intellij.psi.impl.source.tree.PsiCommentImpl;
-import com.intellij.psi.impl.source.tree.java.JavaFileElement;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Test extends AnAction {
+public class QuestionAnalyzer {
+    private final Project project;
 
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        Project project = anActionEvent.getProject();
-        if (project == null) return;
+    public QuestionAnalyzer(Project project) {
+        this.project = project;
+    }
 
+    public Question[] getAllQuestions() {
         List<PsiFile> files = new ArrayList<>();
         PsiManager psiManager = PsiManager.getInstance(project);
         ProjectFileIndex.getInstance(project).iterateContent(virtualFile -> {
@@ -38,15 +28,15 @@ public class Test extends AnAction {
                 }, virtualFile -> virtualFile.isValid() && virtualFile.getName().endsWith(".java")
         );
 
-        List<PsiCommentImpl> comments = getAllComments(files);
-        int a = 0;
+        Parser parser= new Parser(getAllComments(files));
+        return parser.createQuestionsList().toArray(new Question[0]);
     }
 
     private List<PsiCommentImpl> getAllComments(List<PsiFile> files) {
         List<PsiCommentImpl> comments = new ArrayList<>();
 
         for (PsiFile file : files) {
-            for (PsiElement element : file.getChildren()){
+            for (PsiElement element : file.getChildren()) {
                 dfs(element, comments);
             }
         }
@@ -54,7 +44,8 @@ public class Test extends AnAction {
     }
 
     private void dfs(PsiElement element, List<PsiCommentImpl> comments) {
-        if (element.getClass() == PsiCommentImpl.class) {
+        if (element.getClass() == PsiCommentImpl.class
+                && ((PsiCommentImpl) element).getTokenType().getIndex() == 1919) {
             comments.add((PsiCommentImpl) element);
         }
 
